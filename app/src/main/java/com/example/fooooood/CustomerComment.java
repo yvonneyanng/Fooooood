2fcc4a5f2283312400ec4feee218d2fd37c53f21
YauCYin <yau.cyin@gmail.com>
2022-05-06 02:26:18 +0800

    add CustomerComment

---
+++ app/src/main/java/com/example/fooooood/CustomerComment.java
@@ -0,0 +1,14 @@
+package com.example.fooooood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerComment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_comment);
+    }
+}
