f5d7499ec67e22e3fc2f0b3abdde53228dd50eec
yvonne <yvnyang@icloud.com>
2022-05-13 21:06:09 +0800

    "GO!" button not working

--- app/src/main/java/com/example/fooooood/LoginActivity.java
+++ app/src/main/java/com/example/fooooood/LoginActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
         import android.content.Intent;
         import android.os.Bundle;
         import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ToggleButton;
 
@@ -14,6 +15,7 @@ public class LoginActivity extends AppCompatActivity {
     ToggleButton customer;
     ToggleButton restaurant;
     ToggleButton delivery;
+    int choice = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,28 +32,37 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     ToggleButton.OnCheckedChangeListener changeChecker = new ToggleButton.OnCheckedChangeListener() {
-
         @Override
         public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
             if (b){
                 if (compoundButton == customer) {
                     restaurant.setChecked(false);
                     delivery.setChecked(false);
+                    choice = 1;
                 }
                 if (compoundButton == restaurant) {
                     customer.setChecked(false);
                     delivery.setChecked(false);
+                    choice = 2;
                 }
                 if (compoundButton == delivery) {
                     restaurant.setChecked(false);
                     customer.setChecked(false);
+                    choice = 3;
                 }
             }
         }
     };
 
-    public void toHome(View view) {
-        Intent intent=new Intent(this,HomeActivity.class);
-        startActivity(intent);
+    public void onGo(View v){
+        if(choice == 1){
+            Intent customerIntent = new Intent();
+            customerIntent.setClass(this, HomeActivity.class);
+            startActivity(customerIntent);
+        }else if(choice == 2){
+            Intent restaurantIntent = new Intent();
+            restaurantIntent.setClass(this, RestaurantMainActivity.class);
+            startActivity(restaurantIntent);
+        }
     }
 }
