80c43121b3908d98d9627f98da0ac305141909f7
YauCYin <yau.cyin@gmail.com>
2022-05-17 12:40:40 +0800

    driverPage

---
+++ app/src/main/java/com/example/fooooood/DriverNewOrder.java
@@ -0,0 +1,14 @@
+package com.example.fooooood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DriverNewOrder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_new_order);
+    }
+}
