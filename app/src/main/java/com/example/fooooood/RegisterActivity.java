9dd68f84d34513db74e01b23c0817f023a7ab603
D0886112 <q04udl496060@gmail.com>
2022-06-02 23:13:20 +0800

    Login bug處理 && register編寫

--- app/src/main/java/com/example/fooooood/RegisterActivity.java
+++ app/src/main/java/com/example/fooooood/RegisterActivity.java
@@ -4,18 +4,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterActivity extends AppCompatActivity implements OnCompleteListener {
     ToggleButton customer;
     ToggleButton restaurant;
     ToggleButton delivery;
     int toggle = 0;
 
+    private EditText etAccount;
+    private EditText etPassword;
+    private EditText etName;
+    private EditText etPhone;
+    private EditText etSName;
+    private EditText etSPhone;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +43,15 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         toggle = R.id.toggleButton;
+
+        etAccount = findViewById(R.id.textInputAccount);
+        etPassword = findViewById(R.id.textInputPassword);
+        etName = findViewById(R.id.textInputName);
+        etPhone = findViewById(R.id.textInputPhone);
+        etSName = findViewById(R.id.textInputStoreName);
+        etSPhone = findViewById(R.id.textInputStorePhone);
+
+        firebaseAuth = FirebaseAuth.getInstance();
 
         customer = (ToggleButton) findViewById(R.id.toggleButton);
         restaurant = (ToggleButton) findViewById(R.id.toggleButton3);
@@ -79,7 +108,53 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     public void register(View v) {
+        String account = etAccount.getText().toString();
+        String password = etPassword.getText().toString();
 
+        firebaseAuth.createUserWithEmailAndPassword(account, password)
+                .addOnCompleteListener(this, this);
+    }
+
+    @Override
+    public void onComplete(@NonNull Task task) {
+        if (task.isSuccessful()) {
+            Toast.makeText(this, "註冊成功", Toast.LENGTH_LONG).show();
+            addUser();
+        } else {
+            Toast.makeText(this, "註冊失敗", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void addUser() {
+        FirebaseUser curUser = FirebaseAuth.getInstance().getCurrentUser();
+        String account = etAccount.getText().toString();
+        String phone = etPhone.getText().toString();
+        String name = etName.getText().toString();
+        String storeName = etSName.getText().toString();
+        String storePhone = etSPhone.getText().toString();
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        DatabaseReference userRef = db.getReference("user");
+        String className;
+
+        if (toggle == R.id.toggleButton) {//custom
+            className = "custom";
+        } else if (toggle == R.id.toggleButton2) {
+            className = "restaurant";
+        } else {//delivery
+            className = "delivery";
+        }
+        DatabaseReference classRef = userRef.child(className);
+        DatabaseReference uidRef= classRef.child(curUser.getUid());
+        Map<String, Object> user = new HashMap<>();
+        user.put("account", account);
+        user.put("phone", phone);
+        user.put("name", name);
+        if (toggle == R.id.toggleButton2) {
+            user.put("storeName", storeName);
+            user.put("storePhone", storePhone);
+        }
+        uidRef.updateChildren(user);
+        finish();
     }
 
     public void goBack(View v) {
