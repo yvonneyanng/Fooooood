764bbc72c277586e8b55f462079346321f98ac57
yvonne <yvnyang@icloud.com>
2022-06-10 01:31:49 +0800

    delivery-new order

--- app/src/main/java/com/example/fooooood/DriverHome.java
+++ app/src/main/java/com/example/fooooood/DriverHome.java
@@ -2,7 +2,14 @@ package com.example.fooooood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import java.sql.Driver;
 
 public class DriverHome extends AppCompatActivity {
 
@@ -10,5 +17,24 @@ public class DriverHome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_home);
+        ToggleButton onOff = findViewById(R.id.toggleButton4);
+        onOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean on = ((ToggleButton)view).isChecked();
+
+                if(on) {
+                    Toast.makeText(DriverHome.this, "您已上線", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent();
+                    intent.setClass(DriverHome.this, DriverNewOrder.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(DriverHome.this, "您已下線", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        if(getIntent().getIntExtra("on", 0) == 1){
+            onOff.setChecked(true);
+        }
     }
 }
--- app/src/main/java/com/example/fooooood/DriverNewOrder.java
+++ app/src/main/java/com/example/fooooood/DriverNewOrder.java
@@ -2,7 +2,12 @@ package com.example.fooooood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class DriverNewOrder extends AppCompatActivity {
 
@@ -10,5 +15,42 @@ public class DriverNewOrder extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_new_order);
+
+        Button accept = findViewById(R.id.button6);
+        Button decline = findViewById(R.id.button7);
+        Button complete = findViewById(R.id.button9);
+        ImageView back = findViewById(R.id.back);
+        accept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                complete.setVisibility(View.VISIBLE);
+                accept.setVisibility(View.GONE);
+                decline.setVisibility(View.GONE);
+            }
+        });
+        decline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                accept.setVisibility(View.GONE);
+                decline.setVisibility(View.GONE);
+                Toast.makeText(DriverNewOrder.this, "您已拒絕此訂單", Toast.LENGTH_SHORT).show();
+            }
+        });
+        complete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                complete.setVisibility(View.GONE);
+                Toast.makeText(DriverNewOrder.this, "您已完成此訂單", Toast.LENGTH_SHORT).show();
+            }
+        });
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(DriverNewOrder.this, DriverHome.class);
+                intent.putExtra("on", 1);
+                startActivity(intent);
+            }
+        });
     }
 }
